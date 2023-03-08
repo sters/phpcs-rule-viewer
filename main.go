b0fd1438b0f857bd0353a4562cb06ad20936a9bc
sters <sters@users.noreply.github.com>
2023-03-09 02:15:13 +0900

    try

--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/xml"
 	"fmt"
+	"html/template"
 	"io"
 	"log"
 	"os"
@@ -15,8 +16,8 @@ import (
 )
 
 type targetRepository struct {
-	repositoryName string
-	repositoryURL  string
+	RepositoryName string
+	RepositoryURL  string
 	ruleSetDir     string
 }
 
@@ -39,7 +40,8 @@ type RuleSet struct {
 	References  []RuleSetReference `xml:"rule"`
 	Arg         CommonNameValue    `xml:"arg"`
 
-	Rules []*Rule
+	Rules            []*Rule
+	TargetRepository targetRepository
 }
 
 type RuleSetReference struct {
@@ -56,14 +58,14 @@ type Rule struct {
 	Name           string
 	Title          string
 	Description    string
-	CodeComparison RuleCodeComparison
+	CodeComparison []RuleCodeComparison
 }
 
 func (r *Rule) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
 	tmp := struct {
-		Title          string             `xml:"title,attr"`
-		Description    string             `xml:"standard"`
-		CodeComparison RuleCodeComparison `xml:"code_comparison"`
+		Title          string               `xml:"title,attr"`
+		Description    string               `xml:"standard"`
+		CodeComparison []RuleCodeComparison `xml:"code_comparison"`
 	}{}
 	err := d.DecodeElement(&tmp, &start)
 	if err != nil {
@@ -104,6 +106,8 @@ func (r *RuleCode) UnmarshalXML(d *xml.Decoder, start xml.StartElement) error {
 	r.Body = strings.TrimPrefix(r.Body, "\n        \n")
 	r.Body = strings.TrimSuffix(r.Body, "\n        ")
 	r.Body = strings.TrimSuffix(r.Body, "        ")
+	r.Body = strings.ReplaceAll(r.Body, "<em>", "")
+	r.Body = strings.ReplaceAll(r.Body, "</em>", "")
 
 	return nil
 }
@@ -113,52 +117,76 @@ func getTempDir(reponame string) string {
 }
 
 func main() {
+	ruleSets := make([]*RuleSet, 0)
 	for _, target := range targets {
-		log.Printf("%s: Cloning", target.repositoryName)
-		tempDir := getTempDir(target.repositoryName)
+		log.Printf("%s: Cloning", target.RepositoryName)
+		tempDir := getTempDir(target.RepositoryName)
 
 		cmd := exec.Command(
 			"git",
 			"clone",
 			"--depth", "1",
-			target.repositoryURL,
+			target.RepositoryURL,
 			tempDir,
 		)
 		cmdOut, _ := cmd.StdoutPipe()
 		cmdErr, _ := cmd.StderrPipe()
 		err := cmd.Start()
 		if err != nil {
-			panic(err)
+			log.Fatalf("err: %v", err)
 		}
 		rr, _ := io.ReadAll(cmdOut)
-		log.Printf("%s: out: %s", target.repositoryName, string(rr))
+		log.Printf("%s: out: %s", target.RepositoryName, string(rr))
 
 		rr, _ = io.ReadAll(cmdErr)
-		log.Printf("%s: error: %s", target.repositoryName, string(rr))
+		log.Printf("%s: error: %s", target.RepositoryName, string(rr))
 
 		ruleSetDir := filepath.Join(tempDir, target.ruleSetDir)
 		files, err := os.ReadDir(ruleSetDir)
 		if err != nil {
-			panic(err)
+			log.Fatalf("err: %v", err)
 		}
 
 		for _, file := range files {
-			ruleSet, err := getRuleSet(filepath.Join(ruleSetDir, file.Name()))
+			ruleSet, err := getRuleSet(file.Name(), filepath.Join(ruleSetDir, file.Name()))
 			if err != nil {
 				continue
 			}
+			ruleSet.TargetRepository = target
+			ruleSets = append(ruleSets, ruleSet)
 			spew.Dump(ruleSet)
 		}
 	}
+
+	t, err := template.New("index.html").Funcs(template.FuncMap{
+		"noescape": func(s string) template.HTML {
+			return template.HTML(s)
+		},
+	}).ParseFiles("template/index.html")
+	if err != nil {
+		log.Fatalf("err: %v", err)
+	}
+
+	writer, err := os.Create("build/temp.html")
+	if err != nil {
+		log.Fatalf("err: %v", err)
+	}
+	defer writer.Close()
+
+	if err := t.Execute(writer, map[string]interface{}{
+		"ruleSets": ruleSets,
+	}); err != nil {
+		log.Fatalf("err: %v", err)
+	}
 }
 
-func getRuleSet(ruleSetDir string) (*RuleSet, error) {
+func getRuleSet(ruleSetName string, ruleSetDir string) (*RuleSet, error) {
 	reader, err := os.Open(filepath.Join(ruleSetDir, "ruleSet.xml"))
 	if err != nil {
 		return nil, failure.Wrap(err)
 	}
-
 	defer reader.Close()
+
 	result := RuleSet{}
 	dec := xml.NewDecoder(reader)
 	err = dec.Decode(&result)
@@ -166,7 +194,7 @@ func getRuleSet(ruleSetDir string) (*RuleSet, error) {
 		return nil, failure.Wrap(err)
 	}
 
-	result.Rules, err = getRules(ruleSetDir)
+	result.Rules, err = getRules(ruleSetName, ruleSetDir)
 	if err != nil {
 		return nil, failure.Wrap(err)
 	}
@@ -174,7 +202,7 @@ func getRuleSet(ruleSetDir string) (*RuleSet, error) {
 	return &result, nil
 }
 
-func getRules(ruleSetDir string) ([]*Rule, error) {
+func getRules(ruleSetName string, ruleSetDir string) ([]*Rule, error) {
 	docsDir := filepath.Join(ruleSetDir, "Docs")
 	dirs, err := os.ReadDir(docsDir)
 	if err != nil {
@@ -203,6 +231,13 @@ func getRules(ruleSetDir string) ([]*Rule, error) {
 			if err != nil {
 				return nil, failure.Wrap(err)
 			}
+
+			result.Name = fmt.Sprintf(
+				"%s.%s.%s",
+				ruleSetName,
+				dir.Name(),
+				strings.TrimSuffix(file.Name(), "Standard.xml"),
+			)
 
 			rules = append(rules, result)
 		}
